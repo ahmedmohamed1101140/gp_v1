a81ba4d1ccdbc348c9c2954186fe56515ceac7e0
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-03-09 20:57:02 +0200

    emails sents

--- app/controllers/EmailController.js
+++ app/controllers/EmailController.js
@@ -1,7 +1,13 @@
 var nodemailer = require("nodemailer");
 var Department = require("../../models/department");
 var User       = require("../../models/user");
+var Groups     = require("../../models/group");
 var MailController = {};
+
+
+MailController.get_all_mails = function(req,res,next){
+    res.render("Email/index");
+}
 
 MailController.display_creation_form = function(req,res,next){
     Department.find().select("name").exec(function(err,departments){
@@ -18,22 +24,60 @@ MailController.display_creation_form = function(req,res,next){
                     res.redirect("/mails");
                 }
                 else{
-                    console.log(departments);
-                    console.log(users);
-                    res.render("Emails/new" , {departments:departments , users:users});    
+                    Groups.find().select("name").exec(function(err,groups){
+                        if(err){
+                            console.log(err.message);
+                            req.flash("error" , "Sorry Server Error!");
+                            res.redirect("/mails");
+                        }
+                        else{
+                            console.log(groups)
+                            console.log(departments);
+                            console.log(users);
+                            res.render("Emails/new" , {departments:departments , users:users , groups:groups});
+                        }
+                    });    
                 }
             });
         }
     });
 }
 
-MailController.get_all_mails = function(req,res,next){
-    res.render("Email/index");
-}
-
 MailController.send_new_mail = function(req,res,next){
     console.log(req.body);
-    res.send("ahmed");
+    var transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: process.env.MY_EMAIL,
+            pass: process.env.MY_PASSWORD
+        }
+    });
+
+    var maillist = [
+        'ahmedmohamed1101140@outlook.com',
+        'esammohamed17121996@gmail.com',
+      ];
+      maillist.push(req.body.email);
+      console.log(maillist);
+
+    var mailOptions = {
+        from: 'ahmedmohamed1101140@gmail.com',
+        subject: req.body.subject,
+        text: req.body.content,
+        cc: "*******",
+        to: maillist
+    };
+    
+    transport.sendMail(mailOptions , function(err , info){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("mail Sent " +info.response);
+            res.redirect("/mails");
+        }
+    });
+    
 }
 
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app     = express();
-var user      = require("./users");
+const express  = require("express");
+const app      = express();
+var user       = require("./users");
 var department = require("./department");
 var emails     = require("./email");
-var group = require("./group");
-var post = require("./post");
-var comment = require("./comment");
+var group      = require("./group");
+var post       = require("./post");
+var comment    = require("./comment");
 
 app.get("/",function(req,res){
         res.render("index");
