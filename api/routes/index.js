ef8fea957e4eb6b5dc40f3b273139ee0b23a9e93
ahmedmohamed1101140 <ahmedmohamed1101140@outlook.com>
2018-03-10 21:37:33 +0200

    flash messages

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app     = express();
 var user      = require("./users");
 var department = require("./department");
+var emails     = require("./email");
 
 app.use("/users", user);
 app.use("/departments",department);
+app.use("/mails",emails);
 
 module.exports = app;
