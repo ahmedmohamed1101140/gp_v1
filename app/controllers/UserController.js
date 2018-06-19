c210102760826becee324e46fea25f58810042e1
georgenabil <georgenabil1194@gmail.com>
2018-06-20 01:32:31 +0200

    dep_id and interface

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -312,13 +312,14 @@ UserController.addstudents_view=function (req,res,next) {
 // creating new  students
 UserController.Seed_all_users=function (req ,res,next) {
 
-    var departemnt_name= req.body.departemnt_name; //"sw";
+     var departemnt_data =req.body.department_data.split(" ");
+    var departemnt_name= departemnt_data[0]; //"sw";
     var studentsCount =req.body.studentscount;
     var year=req.body.year;
     var collage_serial =  req.body.collage_serial.toString(); //1709
     var student_colleage_id;
-   // check_already_exiting_users(studentsCount,departemnt_name,year,collage_serial);
-
+    var departemnt_id=departemnt_data[1] //id:5ca46dada
+    
 
 
     var Users_to_be_add =[];  
@@ -358,7 +359,8 @@ UserController.Seed_all_users=function (req ,res,next) {
                 username: usercid,
                 collage_id: usercid,
                 department_name:departemnt_name,
-                year:year
+                year:year,
+                department_Id:departemnt_id
                 
             }), "password", function (err,user) {
                 if (err) {
@@ -378,7 +380,7 @@ UserController.Seed_all_users=function (req ,res,next) {
    return res.redirect("/Users");
  
     });
-   
+
  }
 
 
